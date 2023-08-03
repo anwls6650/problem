b1e6a39e2654132393467144ea24d3aaee84f85c
kangmujin <anwls6650@gmail.com>
2023-08-03 18:15:00 +0900

    2023-08-03 18:14 commit

---
+++ src/problem/EX10.java
@@ -0,0 +1,30 @@
+package problem;
+
+import java.util.Scanner;
+
+public class EX10 {
+
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int N = sc.nextInt();
+		String s = "";
+		
+		if( 4 <= N || N<=1000) {
+			
+			for( int i = 4; i >= N; i+=4 ) {
+				
+				s+="long";
+			}
+		}
+		
+		s+= "long int";
+		
+		System.out.println(s);
+	
+		
+	    }
+}
